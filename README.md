40dd529b9f5dac6dc27126646347f34d61a46079
Quoc-An Ho <quocanho29@gmail.com>
2017-12-19 00:10:46 +1100

    WIP: Update TODO and adjust the frame handling

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ TODO List:
 - QuickCheck tests
 - FrameSender and FrameReceiver
 - accept and dialStream
+- Catch Stream and Connection errors in frame sender and frame receiver, and
+  send the relevant rst_stream/ goaway frames to the peer.
--- src/Network/Stream/HTTP2/Multiplexer.hs
+++ src/Network/Stream/HTTP2/Multiplexer.hs
@@ -15,29 +15,20 @@ import Network.HPACK
 import Network.HTTP2
 import Network.HTTP2.Priority
 import Network.Stream.HTTP2.Types
+import Network.Stream.HTTP2.EncodeFrame
+import Control.Monad
 
 
--- | Read a frame from a function returning bytes representing a read from the
--- connection.
-receiveInput :: (Int -> IO BS.ByteString) -> IO (Int, Input)
-receiveInput recvN = do    
-    (tid, hdr@FrameHeader{payloadLength, streamId, flags}) <- decodeFrameHeader <$> recvN frameHeaderLength
+-- | Read and decode a frame from the connection.
+receiveFrame :: (Int -> IO BS.ByteString) -> IO (Int, Frame)
+receiveFrame recvN = do    
+    fhdrBytes <- recvN frameHeaderLength
+    let (tid, hdr@FrameHeader{payloadLength}) = decodeFrameHeader fhdrBytes
     pl <- recvN payloadLength
     case decodeFramePayload tid hdr pl of
-         Left h2error -> E.throwIO h2error
-         Right pl'    -> return (frameHeaderLength + payloadLength, mkInput tid streamId pl')
-  where
-    mkInput tid sid pl'
-        | isControl sid       = mkControl pl'
-        | FrameHeaders <- tid = mkHeaders sid pl'
-        | FrameData    <- tid = mkData pl'
-    -- mkControl (SettingsFrame sl) = CSettings bs??? sl
-    -- mkControl (PingFrame bs) = CFrame (pingFrameAck bs)
-    -- mkControl (GoAwayFrame lastProcessedId eid bs) = CFinish
-    -- mkControl (WindowUpdateFrame ws)
-    mkControl = undefined
-    mkHeaders = undefined
-    mkData = undefined
+        Left h2error -> E.throwIO h2error
+        Right pl'    -> return (frameHeaderLength + payloadLength, Frame hdr pl')
+ 
 
 -- | Given a function that sends on the connection, encode the frame and send.
 --   TODO: replace with ByteString Builder implementation as this uses
@@ -48,43 +39,56 @@ sendOutput send out@(OHeaders sid flags ohdrs@HeadersFrame{}) = do
 sendOutput send out@(OData sid flags odata@DataFrame{}) = do 
     send $ encodeFrame (EncodeInfo flags sid Nothing) odata
 
--- | For a given connection and connection context, receive an incoming frame
--- on that connection. Only 1 of these should be run per connection, as some of
--- the stored data (IORef stuff) is not thread safe. 
+
+-- | For a given connection and connection context, receive and process an 
+-- incoming frame. Only 1 of these should be run per connection, as some of
+-- the stored data in the context (IORef stuff) is not thread safe. 
+-- TODO: whatever calls this needs to catch stream and connection errors that
+-- this throws
 frameReceiver :: Context -> (Int -> IO BS.ByteString) -> IO ()
 frameReceiver ctx@Context{..} connReceive = do
-    (readlen, inframe) <- receiveInput connReceive
+    (frameLen, inframe@Frame{..}) <- receiveFrame connReceive
+    let FrameHeader{..} = frameHeader
     -- Update connection window size by bytes read
-    atomically $ modifyTVar' connectionWindow (readlen-) 
-    case inframe of
-        frameControl@(IControl ctl) -> control ctl
-        frameHeaders@(IHeaders sid hdrblk) -> headers sid hdrblk
-        frameData@(IData sid pl) -> stream sid pl
-  where
-    control = undefined
-    headers = undefined
-    stream = undefined
-    -- headers sid hdrblk= do
-    --     -- Need to read the stream headers and make a new stream
-    --     readIORef clientStreamId
-    --     when (sid <= clientStreamId) $ do
-    --         E.throwIO $ ConnectionError ProtocolError
-    --                   $ "New client stream identifier must not decrease"
-    --     Settings{initialWindowSize} <- readIORef http2settings 
-    --     strm <- newStream sid initialWindowSize
-    --     insert streamTable sid strm
-    --     opened ctx strm
-    -- stream sid pl= do
-    --     mstrm <- search streamTable sid
-    --     case mstrm of
-    --         Nothing -> do
-    --             E.throwIO $ ConnectionError ProtocolError "Stream not open"
-    --         Just strm@(Stream{streamState}) -> do
-    --             state <- readIORef streamState
-    --             case state of
-    --                 Open (Body q clen blen) -> atomically $ writeTQueue q pl
-    --                 -- First frame of the stream after stream headers
-    --                 Open JustOpened -> undefined
+    atomically $ modifyTVar' connectionWindow (frameLen-) 
+    -- TODO: Update stream window size by bytes read
+    case framePayload of
+        -- Receiving a data frame
+        -- Pass it to the correct stream
+        DataFrame pl -> do
+            mstrm <- search streamTable streamId
+            case mstrm of
+                Nothing -> do
+                    -- The stream is not open or half closed, so cannot receive
+                    -- data.
+                    E.throwIO $ StreamError StreamClosed streamId
+                Just strm@(Stream{streamState}) -> do
+                    state <- readIORef streamState
+                    case state of
+                        Open (Body q clen blen) -> atomically $ writeTQueue q pl
+                        -- First frame of the stream after stream headers
+                        Open JustOpened -> undefined
+        HeadersFrame mpri hdrblk -> do
+            -- Need to read the stream headers and make a new stream
+            csid <- readIORef clientStreamId
+            when (csid > streamId) $ do
+                E.throwIO $ ConnectionError ProtocolError
+                          $ "New client stream identifier must not decrease"
+            Settings{initialWindowSize} <- readIORef http2settings 
+            strm <- newStream streamId initialWindowSize
+            insert streamTable streamId strm
+            opened ctx strm
+     
+        PriorityFrame pri -> undefined
+        RSTStreamFrame eid -> undefined
+        SettingsFrame sl -> undefined
+        PushPromiseFrame sid hdrblk -> undefined
+        PingFrame bs -> undefined
+        GoAwayFrame lastStreamId eid bs -> undefined
+        WindowUpdateFrame ws -> undefined
+        ContinuationFrame hdrblk -> undefined
+        UnknownFrame tid bs -> undefined
+
 
 -- | read an output frame from the connection context, and then send the frame
 -- using the connection sender
